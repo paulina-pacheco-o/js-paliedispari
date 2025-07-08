d21482fece3854b7e18b556f49a9e0f06bf9591c
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-08 09:03:56 +0200

    prompt and first function second exercise

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 //PALINDROMA
-const word = prompt(`Inserisci una parola`);
+//const word = prompt(`Inserisci una parola`);
 
-function paliWord(parola) {
-  const parolaInvertita = parola.split("").reverse().join("")
-  const result = parola === parolaInvertita
-  return result
+//function paliWord(parola) {
+//const parolaInvertita = parola.split("").reverse().join("")
+//const result = parola === parolaInvertita
+//return result
+//}
+
+//const check = paliWord(word)
+//if (check) {
+//console.log(`Questa parola è palindroma`)
+//}
+//else {
+//console.log(`Questa parola non è palindroma`)
+//}
+
+
+//PARI E DISPARI
+const userNumber = parseInt(prompt(`Insersci un numero da 1 a 5`));
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1));
 }
 
-const check = paliWord(word)
-if (check) {
-  console.log(`Questa parola è palindroma`)
-}
-else {
-  console.log(`Questa parola non è palindroma`)
-}
+console.log(generateRandomNumber(1, 5));
